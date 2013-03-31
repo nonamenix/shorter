151c94062d955eb523820da3474329ab57406656
nonamenix <nonamenix@gmail.com>
2013-03-31 13:43:10 -0700

    небольшой makeup для нашего приложения

--- shorter/urls.py
+++ shorter/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^success/(?P<slug>.*)$', DetailView.as_view(slug_field="short_urn",
         model=Short, context_object_name="short"), name='success'),
     url(r'^$', ListView.as_view(model=Short, context_object_name="shorts")),
-    url(r'^(?P<short_urn>.*)$', ShortRedirectView.as_view()),
+    url(r'^(?P<short_urn>.*)$', ShortRedirectView.as_view(), name='go'),
 
 
     # Examples:
